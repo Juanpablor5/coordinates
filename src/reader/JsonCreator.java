dfbbbe6a288a54e2ea4b1586258d2209443b9592
Juan <jpr.arango10@uniandes.edu.co>
2020-03-20 21:22:21 -0500

    Json

---
+++ src/reader/JsonCreator.java
@@ -0,0 +1,70 @@
+package reader;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class JsonCreator {
+
+	public static void main(String[] args) {
+		String rutaEti = "data/etiquetas.csv";
+		String[] eti = new String[82];
+		
+		String csvFile = "data/cat.csv";
+		BufferedReader br = null;
+		String line = "";
+		String cvsSplitBy = ";";
+
+		try {
+			int c=0;					
+			br = new BufferedReader(new FileReader(rutaEti));
+			while ((line = br.readLine()) != null) {
+				String[] separado = line.split(cvsSplitBy);
+				eti[c] = separado[1];				
+				c++;
+			}
+			br.close();
+			
+			c=0;
+			br = new BufferedReader(new FileReader(csvFile));
+			String cadena = "";
+			cadena += "{\"categorías\": [";
+			while ((line = br.readLine()) != null) {				
+				String[] separado = line.split(cvsSplitBy);
+				String[] etiquetas =  separado[1].split(",");
+				cadena += "{\"id\":\""+ separado[0] +"\"";
+				cadena += ",\"etiquetas\":[{";
+				for (int i = 0; i < etiquetas.length; i++) {					
+					cadena += "\"etiqueta"+(i+1)+"\":{";					
+					cadena += "\"id\":\""+Integer.parseInt(etiquetas[i])+"\",";					
+					cadena += "\"nombre\":\""+ eti[Integer.parseInt(etiquetas[i])-1] +"\"";					
+					cadena += "}";					
+					if (i != etiquetas.length-1)
+						cadena += ",";
+				}
+				cadena += "}],";
+				cadena += "\"categoría\":\""+ separado[2] +"\"}";
+				if (c != 270)
+					cadena += ",";
+				c++;
+			}
+			cadena += "]}";
+			System.out.println(cadena);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+}
